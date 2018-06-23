ca2fed37aaa25ad984d8c1a2ee14917cf4bb1ffb
Christopher Brown <io@henrian.com>
2018-06-23 13:46:48 -0500

    Use markdown format in package's long_description

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     version='0.3.0',
     url='https://github.com/chbrown/liwc-python',
     description='Linguistic Inquiry and Word Count (LIWC) analyzer (proprietary data not included)',
-    long_description=open('README.rst').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     license='MIT',
     author='Christopher Brown',
     author_email='chrisbrown@utexas.edu',
     packages=['liwc'],
-    zip_safe=True
+    zip_safe=True,
 )
