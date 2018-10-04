0a8a718b6cf09770e055be9ed99c9039c1199d20
Christopher Brown <io@henrian.com>
2018-10-04 00:00:56 -0500

    Move setup config into separate ini file

--- setup.py
+++ setup.py
@@ -1,15 +1,1 @@
-from setuptools import setup
-
-setup(
-    name='liwc',
-    version='0.3.0',
-    url='https://github.com/chbrown/liwc-python',
-    description='Linguistic Inquiry and Word Count (LIWC) analyzer (proprietary data not included)',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    license='MIT',
-    author='Christopher Brown',
-    author_email='chrisbrown@utexas.edu',
-    packages=['liwc'],
-    zip_safe=True,
-)
+from setuptools import setup; setup()  # see setup.cfg
