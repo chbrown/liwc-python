f5ead13fedc50ff04ea7aa92a577386dd0a79f1e
Christopher Brown <io@henrian.com>
2018-10-12 15:27:01 -0500

    Read test resource dynamically (i.e., relative to module)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,18 @@
+import os.path
 import unittest
 
 import liwc
 
+test_dir = os.path.dirname(__file__)
+
 
 class TestAlphaDic(unittest.TestCase):
     def test_category_names(self):
-        _, category_names = liwc.load_token_parser('tests/alpha.dic')
+        _, category_names = liwc.load_token_parser(os.path.join(test_dir, 'alpha.dic'))
         self.assertEqual(category_names, ['A', 'Bravo'])
 
     def test_parse(self):
-        parse, _ = liwc.load_token_parser('tests/alpha.dic')
+        parse, _ = liwc.load_token_parser(os.path.join(test_dir, 'alpha.dic'))
         sentence = 'Any alpha a bravo charlie Bravo boy'
         tokens = sentence.split()
         matches = [category for token in tokens for category in parse(token)]
