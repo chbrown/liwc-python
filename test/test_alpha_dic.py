ed30b31eee4d572c0d72a37faabcd8c2be402abe
Christopher Brown <io@henrian.com>
2019-12-08 22:01:10 -0500

    Blacken

--- test/test_alpha_dic.py
+++ test/test_alpha_dic.py
@@ -6,14 +6,14 @@ test_dir = os.path.dirname(__file__)
 
 
 def test_category_names():
-    _, category_names = liwc.load_token_parser(os.path.join(test_dir, 'alpha.dic'))
-    assert category_names == ['A', 'Bravo']
+    _, category_names = liwc.load_token_parser(os.path.join(test_dir, "alpha.dic"))
+    assert category_names == ["A", "Bravo"]
 
 
 def test_parse():
-    parse, _ = liwc.load_token_parser(os.path.join(test_dir, 'alpha.dic'))
-    sentence = 'Any alpha a bravo charlie Bravo boy'
+    parse, _ = liwc.load_token_parser(os.path.join(test_dir, "alpha.dic"))
+    sentence = "Any alpha a bravo charlie Bravo boy"
     tokens = sentence.split()
     matches = [category for token in tokens for category in parse(token)]
     # matching is case-sensitive, so the only matches are "alpha" (A), "a" (A) and "bravo" (Bravo)
-    assert matches == ['A', 'A', 'Bravo']
+    assert matches == ["A", "A", "Bravo"]
